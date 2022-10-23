4cb6ad2c8df27086763981ae3ed4e4cd80099be6
Selim Okten <selimoktenofficial313@gmail.com>
2022-10-23 17:59:42 +0300

    Files Update

--- lib/components/reusable_text_button.dart
+++ lib/components/reusable_text_button.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart' hide BoxDecoration, BoxShadow;
 import 'package:calculator/constants.dart';
+import 'package:flutter_inset_box_shadow/flutter_inset_box_shadow.dart';
 
 class ReusableTextButton extends StatelessWidget {
   ReusableTextButton({required this.text, required this.style});
@@ -9,14 +10,35 @@ class ReusableTextButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100),
+        boxShadow: [
+          for (double i = 0; i < 4; i++)
+            BoxShadow(
+              color: Colors.white,
+              blurRadius: 2 * i,
+              inset: true,
+            )
+        ],
+      ),
       child: TextButton(
         style: style,
-          onPressed:() {
-            
-          },
-          child: Text(text, style: kTextStyle,),),
+        onPressed: () {},
+        child: Text(
+          text,
+          style: kTextStyle,
+        ),
+      ),
     );
+  }
+}
+
+class NeonButton extends StatelessWidget {
+  const NeonButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
